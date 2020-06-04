a9607c247d8fd1e4cd401a5bd71e0521ab6bc894
yunti <yunti9@icloud.com>
2020-06-04 22:23:12 +0100

    Add useState stub

--- core.js
+++ core.js
@@ -1,18 +1,5 @@
 "use strict";
 
-const add = (function getAdd() {
-  let foo = 1;
-  return function () {
-    foo += 1;
-    return foo;
-  };
-})();
-
-const add = getAdd();
-console.log(add());
-console.log(add());
-console.log(add());
-console.log(add());
-// now with a closure state is private and not reset
-foo = 45;
-console.log(add());
+function useState(initVal) {
+  return [state, setState];
+}
