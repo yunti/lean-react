181b562ec174551b985133bd862b8263a290f981
yunti <yunti9@icloud.com>
2020-06-04 23:01:14 +0100

    Quick fix to useState so state isn't reset

--- core.js
+++ core.js
@@ -2,12 +2,12 @@
 
 function useState(initVal) {
   let _val = initVal;
-  const state = _val;
+  const state = () => _val;
   const setState = (newVal) => (_val = newVal);
   return [state, setState];
 }
 
 const [count, setCount] = useState(1);
-console.log(count);
-useState(2);
-console.log(count);
+console.log(count());
+setCount(2);
+console.log(count());
