560bedfdc572bf83072dda1c9fb28792e9d5efb2
yunti <yunti9@icloud.com>
2020-06-04 23:08:23 +0100

    Create React module

--- core.js
+++ core.js
@@ -1,13 +1,16 @@
 "use strict";
 
-function useState(initVal) {
-  let _val = initVal;
-  const state = () => _val;
-  const setState = (newVal) => (_val = newVal);
-  return [state, setState];
-}
+const React = (function () {
+  function useState(initVal) {
+    let _val = initVal;
+    const state = () => _val;
+    const setState = (newVal) => (_val = newVal);
+    return [state, setState];
+  }
+  return { useState };
+})();
 
-const [count, setCount] = useState(1);
+const [count, setCount] = React.useState(1);
 console.log(count());
 setCount(2);
 console.log(count());
