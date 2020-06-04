4a59baa224137d8262a09765b003078e1ffa2938
yunti <yunti9@icloud.com>
2020-06-04 22:45:01 +0100

    add usestate but has bug

--- core.js
+++ core.js
@@ -1,5 +1,13 @@
 "use strict";
 
 function useState(initVal) {
+  let _val = initVal;
+  const state = _val;
+  const setState = (newVal) => (_val = newVal);
   return [state, setState];
 }
+
+const [count, setCount] = useState(1);
+console.log(count);
+useState(2);
+console.log(count);
