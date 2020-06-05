dab89475351e3463035bca3eb4fcb6fec2948183
yunti <yunti9@icloud.com>
2020-06-05 12:59:11 +0100

    Add setText hook to component

--- core.js
+++ core.js
@@ -17,12 +17,16 @@ const React = (function () {
 
 function Component() {
   const [count, setCount] = React.useState(1);
+  const [text, setText] = React.useState("apple");
   return {
-    render: () => console.log(count),
+    render: () => console.log({ count, text }),
     click: () => setCount(count + 1),
+    type: (word) => setText(word),
   };
 }
-// bug: logging state getter not state
+// bug: setText reuses state from setCount
 var App = React.render(Component);
 App.click();
+var App = React.render(Component);
+App.type("pear");
 var App = React.render(Component);
